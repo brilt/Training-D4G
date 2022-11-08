e57e14c30f734d998b7b788e3d339827d7e8a69a
BethGarion <tperrault.ing2024@esaip.org>
2022-11-08 17:23:53 +0100

    some change dont ask questions

--- test.js
+++ test.js
@@ -28,3 +28,47 @@ function Panier()
         if (index > -1) this.liste.splice(index, 1);
     }
 }
+
+var button = document.getElementById("button");
+var select = document.getElementById("select");
+var basket = document.getElementById("basket");
+var totalCost = document.getElementById("total-cost");
+
+function addToBasket() {
+ var li = document.createElement("li");
+ li.innerHTML = select.options[select.selectedIndex].text;
+ basket.appendChild(li);
+
+}
+
+document.getElementById("remove-button").onclick = function() {
+var list = document.getElementById("basket");
+list.removeChild(list.childNodes[0])
+
+}
+
+
+button.addEventListener("click", addToBasket);
+
+
+
+var button = document.getElementById("button");
+var select = document.getElementById("select");
+var basket = document.getElementById("basket");
+var totalCost = document.getElementById("total-cost");
+
+function addToBasket() {
+ var li = document.createElement("li");
+ li.innerHTML = select.options[select.selectedIndex].text;
+ basket.appendChild(li);
+
+}
+
+document.getElementById("remove-button").onclick = function() {
+var list = document.getElementById("basket");
+list.removeChild(list.childNodes[0])
+
+}
+
+
+button.addEventListener("click", addToBasket);
